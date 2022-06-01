1fcfe1d9a02d41c3f50d75590ed14baee7573e8b
Aleksandr <albarmin4549@gmail.com>
2022-06-01 12:30:58 +0300

    добавлена валидация даты

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const dateContainer = document.querySelector('.date__container');
 const video = document.querySelector('video');
 const playBtn = document.querySelector('#play-btn');
 const closeBtn = document.querySelector('#stop-btn');
+const day = document.querySelector('#day');
+const mounth = document.querySelector('#mounth');
+const yare = document.querySelector('#yare');
 
-const checkDate = () => {
-  const day = document.querySelector('#day');
-  const mounth = document.querySelector('#mounth');
-  const yare = document.querySelector('#yare');
-  return !!(day && mounth && yare);
+const validateDate = () => {
+  const dayValue = +day.value;
+  const mounthValue = +mounth.value;
+  const yareValue = +yare.value;
+  debugger;
+  return !!(dayValue >= 1 && dayValue <= 31 && mounthValue <= 12 && mounthValue >= 1 && yareValue <= 2022); 
 };
 
 const startVideo = () => {
-  if (!checkDate()) {
-    console.log(checkDate())
-    return;
-  }
+  if (!validateDate()) return;
 
   videoContainer.classList.remove('hidden');
   dateContainer.classList.add('hidden');
@@ -26,7 +27,48 @@ const stopVideo = () => {
   video.pause();
   videoContainer.classList.add('hidden');
   dateContainer.classList.remove('hidden');
-}
+};
+
+const checkNumberType = (value) => !isNaN(value);
+
+const checkDay = (value) => {
+  return !!(!isNaN(value) && value >= 1 && value <= 31);  
+};
+
+const checkMounth = (value) => {
+  return !!(!isNaN(value) && value >= 1 && value <= 12);  
+};
+
+const checkYare = (value) => {
+  return !!(!isNaN(value) && value <= 2022);  
+};
+
+const dayChange = (e) => {
+  if (!checkDay(e.target.value)) {
+    day.classList.add('error');
+  } else {
+    day.classList.remove('error');
+  }
+};
+
+const mounthChange = (e) => {
+  if (!checkMounth(e.target.value)) {
+    mounth.classList.add('error');
+  } else {
+    mounth.classList.remove('error');
+  }
+};
+
+const yareChange = (e) => {
+  if (!checkYare(e.target.value) || !e.target.value) {
+    yare.classList.add('error');
+  } else {
+    yare.classList.remove('error');
+  }
+};
 
 playBtn.addEventListener('click', startVideo);
 closeBtn.addEventListener('click', stopVideo);
+day.addEventListener('change', dayChange);
+mounth.addEventListener('change', mounthChange);
+yare.addEventListener('change', yareChange);
